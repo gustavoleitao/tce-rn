30e429fc1096d18f8bb13fa00d620408b4cb0102
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2023-02-10 17:24:07 -0300

    adiciona rota para listar usuários

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+  const users = [{username: 'abc67'}, {username: 'j.pin837'}]
+  res.send(users)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
